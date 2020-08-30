f90d7b8fb91d991010b14ad7baa16a4a6c4f2d35
lekai <lekai@ubuntu.debian.lan>
2020-08-31 01:09:16 +0800

    insert plan_date(gen) into brps

--- lpr/icbc.go
+++ lpr/icbc.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/civil"
+	dataframe "github.com/rocketlaunchr/dataframe-go"
 )
 
 func icbc(model BankRepayPlanCalcModel) {
@@ -23,14 +24,44 @@ func (model *BankRepayPlanCalcModel) addIcbcInsPlanDate() *BankRepayPlanCalcMode
 	brps := model.Brps
 	col, _ := brps.NameToColumn("plan_date")
 	se := brps.Series[col]
-	fmt.Print("se:\n", se)
+	// fmt.Print("se:\n", se)
 	startDate := se.Value(0).(civil.Date)
 	endDate := se.Value(se.NRows() - 1).(civil.Date)
-	dates := genIcbcInsPayDate(startDate, endDate)
+	planDateSlice := genIcbcInsPlanDate(startDate, endDate)
+	maps := model.slice2maps("plan_date", planDateSlice...)
+	for _, val := range maps {
+		brps.Append(nil, val)
+	}
+	brps.Sort(context.TODO(), []dataframe.SortKey{
+		{Key: "plan_date", Desc: false},
+	})
 
-	fmt.Printf("\n dates:\n %s", dates)
+	fmt.Print(brps.Table())
 
 	return nil
+
+}
+
+func (model *BankRepayPlanCalcModel) slice2maps(fieldname string, vals ...interface{}) []interface{} {
+	brps := model.Brps
+	names := brps.Names()
+
+	maps := make([]interface{}, 0)
+	if vals == nil {
+		panic("get no vals")
+	} else {
+		for _, val := range vals {
+			// 初始化a
+			a := make(map[string]interface{})
+			for _, name := range names {
+				a[name] = nil
+			}
+			a[fieldname] = val
+			maps = append(maps, a)
+		}
+	}
+	fmt.Printf("maps:\n %+v", maps)
+	return maps
 
 }
 
@@ -38,7 +69,7 @@ func (model *BankRepayPlanCalcModel) addIcbcInsPlanDate() *BankRepayPlanCalcMode
 // 若起始日为21日以后（含21日当日），则生成的第一个扣息日为下月21日
 // 若起始日为21日以前，则生成的第一个扣息日为本月21日
 // 最后一期利随本清,额外生成一个利息还款计划
-func genIcbcInsPayDate(start, end civil.Date) (dates []civil.Date) {
+func genIcbcInsPlanDate(start, end civil.Date) (dates []interface{}) {
 	termMonths := 12*(end.Year-start.Year) + (int(end.Month) - int(start.Month))
 	plus := 0
 	if start.Day >= 21 {
@@ -68,36 +99,3 @@ func genIcbcInsPayDate(start, end civil.Date) (dates []civil.Date) {
 
 	return
 }
-
-/* func genIcbcInsPayDate(start, end civil.Date) (dates []civil.Date) {
-	// year,month 初始化
-	year := start.Year
-	month := start.Month
-	if start.Day >= 21 {
-		if month == time.December {
-			year = year + 1
-			month = time.January
-		} else {
-			month = month + 1
-		}
-	}
-
-	for y := year; y <= end.Year; y++ {
-		for m := time.January; m <= 12; m++ {
-			if y == start.Year {
-				m = month
-			}
-			// 最后一期本金还款日小于21日时，不生成该月21日的利息还款计划
-			if (y == end.Year) && (m == end.Month) && (end.Day < 21) {
-				break
-			}
-			d := civil.Date{Year: y, Month: m, Day: 21}
-			dates = append(dates, d)
-		}
-	}
-	// 最后一期利随本清，单独生成一条利息还款计划
-	dlast := civil.Date{Year: end.Year, Month: end.Month, Day: end.Day}
-	dates = append(dates, dlast)
-	return
-
-} */
